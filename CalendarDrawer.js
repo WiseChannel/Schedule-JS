a2bf2d52d7b70e52f8be605e2fe2072a7b02a4b1
Ilya Wise <ilyaphotoshop17@gmail.com>
2019-02-22 21:25:09 +0200

    BaseCom

--- CalendarDrawer.js
+++ CalendarDrawer.js
@@ -113,7 +113,7 @@ function SetBackgroundCalendarSize (width, height, isPrinting) {
 	
 	if (width != 0) {
 		
-		backgroundCalendarWidth = red;
+		backgroundCalendarWidth = width;
 		document.getElementById("calendarTable").style.width = width + "px";
 		
 		SetBackgroundSize (Math.max (backgroundWidth, width + backgroundCalendarLeft + 10 + (isPrinting ? 800 : 0)), 0);
